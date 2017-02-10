d67927b2a607960d58e7c5f79e393fc32d81442f
Yangsiyoung <Yangsiyoung@YangSiYoung>
2017-02-10 21:06:00 +0900

    Kakao Login

---
+++ app/src/main/java/com/example/yangsiyoung/hellologin/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.yangsiyoung.hellologin;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.auth.helper.Base64;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+
+import java.security.MessageDigest;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private SessionCallback sessionCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_SIGNATURES);
+            for(Signature signature : info.signatures){
+                MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+                messageDigest.update(signature.toByteArray());
+                Log.d("aaaa", Base64.encodeBase64URLSafeString(messageDigest.digest()));
+            }
+        } catch (Exception e){
+            Log.d("error", "PackageInfo error is " + e.toString());
+        }
+
+        sessionCallback = new SessionCallback();
+        Session.getCurrentSession().addCallback(sessionCallback);
+        Session.getCurrentSession().checkAndImplicitOpen();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)){
+            return ;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void request(){
+        UserManagement.requestMe(new MeResponseCallback() {
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.d("error", "Session Closed Error is " + errorResult.toString());
+            }
+
+            @Override
+            public void onNotSignedUp() {
+
+            }
+
+            @Override
+            public void onSuccess(UserProfile result) {
+                Toast.makeText(MainActivity.this, "사용자 이름은 " + result.getNickname(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private class SessionCallback implements ISessionCallback{
+
+        @Override
+        public void onSessionOpened() {
+            request();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.d("error", "Session Fail Error is " + exception.getMessage().toString());
+        }
+    }
+}
